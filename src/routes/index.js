4b1ee7e215f4730413cee732acfd27540a9668bd
f3rn4nd001 <f3rn4nd0.v3ntur4@gmail.com>
2020-02-01 20:00:54 -0600

    envio de datos y mostreo de tabla de libros

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const Usuario = require('../model/Usuarios');
+const Libro = require('../model/Liblos');
 
 router.get('/principal', async(req, res) => {
     const usuarios = await Usuario.find().sort({ nombre: 'desc' });
-    res.render('principal/principal', { usuarios });
-
+    const libros = await Libro.find().sort({ nombreLibro: 'desc' });
+    res.render('principal/principal', { libros, usuarios });
 });
+
+
+
 router.post('/usuario/nuevousuario', async(req, res) => {
     console.log(req.body);
     const { tipo, nombre, apellido, numerocontrol, escuelaProcedencia, calle, telefono, numExt, colonia, cp } = req.body;
@@ -44,7 +48,7 @@ router.post('/usuario/nuevousuario', async(req, res) => {
             errors,
         });
     } else {
-        guardado.push({ text: 'Datos guardados' });
+        guardado.push({ text: 'Datos de usuario guardados' });
         const usuarioNuevo = new Usuario({ tipo, nombre, apellido, telefono, numerocontrol, escuelaProcedencia, calle, numExt, colonia, cp });
         console.log(usuarioNuevo);
         await usuarioNuevo.save();
@@ -56,6 +60,47 @@ router.post('/usuario/nuevousuario', async(req, res) => {
         });
     };
 });
+
+router.post('/libro/nuevouslibro', async(req, res) => {
+    console.log(req.body);
+    const { nombreLibro, autor, editorialLibro, tipoLibro, numeroPaginas } = req.body;
+    const errorsLib = [];
+    const guardadoLib = [];
+    if (!nombreLibro) {
+        errorsLib.push({ text: 'El campo Nombre no puede estar vacio' });
+    }
+    if (!autor) {
+        errorsLib.push({ text: 'El campo autor no puede estar vacio' });
+    }
+    if (!editorialLibro) {
+        errorsLib.push({ text: 'El campo Nombre no puede estar vacio' });
+    }
+    if (!tipoLibro) {
+        errorsLib.push({ text: 'El campo Numero de Control no puede estar vacio' });
+    }
+    if (!numeroPaginas) {
+        errorsLib.push({ text: 'El campo Escuela de Procedencia no puede estar vacio' });
+    }
+    if (errorsLib.length > 0) {
+        res.render('principal/principal', {
+            errorsLib,
+        });
+    } else {
+        guardadoLib.push({ text: 'Datos del libro guardados' });
+        const libroNuevo = new Libro({ nombreLibro, autor, editorialLibro, tipoLibro, numeroPaginas });
+        console.log(libroNuevo);
+        await libroNuevo.save();
+        const libros = await Libro.find().sort({ nombreLibro: 'desc' });
+
+        res.render('principal/principal', {
+            libros,
+            guardadoLib,
+        });
+    };
+});
+
+
+
 router.get('/', (req, res) => {
     res.render('vista/index');
 
